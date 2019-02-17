06315f6f5790351c7e59b63ad96c03a1696e3c90
Di Wen <ifyouseewendy@gmail.com>
2019-02-17 12:55:58 -0500

    Reformat loader to use structued data*
    refer to https://github.com/chinese-poetry/chinese-poetry/blob/master/ci/ci.song.0.json

--- loader.js
+++ loader.js
@@ -1,4 +1,4 @@
-var backgroundColorList = [
+const BACKGROUND_COLOR_LIST = [
     "#86a697",
     "#947A6D",
     "#BC9F77",
@@ -10,33 +10,48 @@ var backgroundColorList = [
     "#6A8372"
 ];
 
-// title poet\n poem, line seperated by \n
-var poemList = [
-    "如梦令（其二）\n 宋 李清照\n昨夜雨疏风骤，浓睡不消残酒。\n试问卷帘人，却道海棠依旧。\n知否？知否？应是绿肥红瘦。",
-    "如梦令（其一）\n 宋 李清照\n常记溪亭日暮，沉醉不知归路。\n兴尽晚回舟，误入藕花深处。\n争渡，争渡，惊起一滩鸥鹭。"
+// https://github.com/chinese-poetry/chinese-poetry/blob/master/ci/ci.song.0.json
+const POEMS = [
+    {
+        "author": "李清照",
+        "paragraphs": [
+            "常记溪亭日暮。",
+            "沈醉不知归路。",
+            "兴尽晚回舟，误入藕花深处。",
+            "争渡。",
+            "争渡。",
+            "惊起一滩鸥鹭。"
+        ],
+        "rhythmic": "如梦令"
+    },
+    {
+        "author": "李清照",
+        "paragraphs": [
+            "昨夜雨疏风骤。",
+            "浓睡不消残酒。",
+            "试问卷帘人，却道海棠依旧。",
+            "知否。",
+            "知否。",
+            "应是绿肥红瘦。"
+        ],
+        "rhythmic": "如梦令",
+    }
 ];
 
 document.addEventListener(
     "DOMContentLoaded",
     function() {
-        // random pick background color
-        var randomColorIndex = Math.floor(
-            Math.random() * backgroundColorList.length
-        );
-        var color = backgroundColorList[randomColorIndex];
-        document.body.style.backgroundColor = color;
+        document.body.style.backgroundColor = getRandom(BACKGROUND_COLOR_LIST);
 
-        // insert poem
-        var randomPoemIndex = Math.floor(Math.random() * poemList.length);
-        var poemString = poemList[randomPoemIndex];
-        var poemLineLists = poemString.split("\n");
-        document.getElementById("title").innerHTML = poemLineLists[0];
-        document.getElementById("poet").innerHTML = poemLineLists[1];
-        var content = "";
-        for (var i = 2; i < poemLineLists.length; i++) {
-            content += "<br/>" + poemLineLists[i];
-        }
-        document.getElementById("contents").innerHTML = content;
+        const poem = getRandom(POEMS);
+        document.getElementById("title").innerHTML = poem.rhythmic;
+        document.getElementById("poet").innerHTML = poem.author;
+        document.getElementById("contents").innerHTML = poem.paragraphs.join("<br/>");
     },
     false
 );
+
+function getRandom(list) {
+    const i = Math.floor(Math.random() * list.length);
+    return list[i];
+}
