c925354bbeed9a9f588526f69a62337024a20e4f
Di Wen <ifyouseewendy@gmail.com>
2019-02-17 13:49:19 -0500

    Add fallback poems when error fetching

--- loader.js
+++ loader.js
@@ -1,3 +1,61 @@
+document.addEventListener(
+    "DOMContentLoaded",
+    function() {
+        document.body.style.backgroundColor = getRandom(BACKGROUND_COLOR_LIST);
+
+        fetch('https://raw.githubusercontent.com/ifyouseewendy/zhifou/master/example/ci.song.0.json')
+            .then(
+                function(response) {
+                    if (response.status == 200) {
+                        return response.json();
+                    } else {
+                        return POEMS;
+                    }
+                }
+            ).then(
+                function(poems) {
+                    const poem = getRandom(poems);
+                    document.getElementById("title").innerHTML = poem.rhythmic;
+                    document.getElementById("poet").innerHTML = poem.author;
+                    document.getElementById("contents").innerHTML = "<br/>" + poem.paragraphs.join("<br/>");
+                }
+            );
+    },
+    false
+);
+
+function getRandom(list) {
+    const i = Math.floor(Math.random() * list.length);
+    return list[i];
+}
+
+const POEMS = [
+  {
+    "author": "李清照",
+    "paragraphs": [
+      "常记溪亭日暮。",
+      "沈醉不知归路。",
+      "兴尽晚回舟，误入藕花深处。",
+      "争渡。",
+      "争渡。",
+      "惊起一滩鸥鹭。"
+    ],
+    "rhythmic": "如梦令"
+  },
+  {
+    "author": "李清照",
+    "paragraphs": [
+      "昨夜雨疏风骤。",
+      "浓睡不消残酒。",
+      "试问卷帘人，却道海棠依旧。",
+      "知否。",
+      "知否。",
+      "应是绿肥红瘦。"
+    ],
+    "rhythmic": "如梦令"
+  }
+];
+
 const BACKGROUND_COLOR_LIST = [
     "#86a697",
     "#947A6D",
@@ -9,27 +67,3 @@ const BACKGROUND_COLOR_LIST = [
     "#566C73",
     "#6A8372"
 ];
-
-document.addEventListener(
-    "DOMContentLoaded",
-    function() {
-        document.body.style.backgroundColor = getRandom(BACKGROUND_COLOR_LIST);
-
-        fetch('https://raw.githubusercontent.com/ifyouseewendy/zhifou/master/example/ci.song.0.json')
-            .then(function(response) {
-                return response.json();
-            }).then(function(poems) {
-                console.log(poems);
-                const poem = getRandom(poems);
-                document.getElementById("title").innerHTML = poem.rhythmic;
-                document.getElementById("poet").innerHTML = poem.author;
-                document.getElementById("contents").innerHTML = "<br/>" + poem.paragraphs.join("<br/>");
-            });
-    },
-    false
-);
-
-function getRandom(list) {
-    const i = Math.floor(Math.random() * list.length);
-    return list[i];
-}
