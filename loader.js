1fc4e1503d31297d21fc632ebde27e09901ec9db
Di Wen <ifyouseewendy@gmail.com>
2019-02-17 13:13:42 -0500

    Load poems from JSON

--- loader.js
+++ loader.js
@@ -10,43 +10,21 @@ const BACKGROUND_COLOR_LIST = [
     "#6A8372"
 ];
 
-// https://github.com/chinese-poetry/chinese-poetry/blob/master/ci/ci.song.0.json
-const POEMS = [
-    {
-        "author": "李清照",
-        "paragraphs": [
-            "常记溪亭日暮。",
-            "沈醉不知归路。",
-            "兴尽晚回舟，误入藕花深处。",
-            "争渡。",
-            "争渡。",
-            "惊起一滩鸥鹭。"
-        ],
-        "rhythmic": "如梦令"
-    },
-    {
-        "author": "李清照",
-        "paragraphs": [
-            "昨夜雨疏风骤。",
-            "浓睡不消残酒。",
-            "试问卷帘人，却道海棠依旧。",
-            "知否。",
-            "知否。",
-            "应是绿肥红瘦。"
-        ],
-        "rhythmic": "如梦令",
-    }
-];
-
 document.addEventListener(
     "DOMContentLoaded",
     function() {
         document.body.style.backgroundColor = getRandom(BACKGROUND_COLOR_LIST);
 
-        const poem = getRandom(POEMS);
-        document.getElementById("title").innerHTML = poem.rhythmic;
-        document.getElementById("poet").innerHTML = poem.author;
-        document.getElementById("contents").innerHTML = poem.paragraphs.join("<br/>");
+        fetch('https://raw.githubusercontent.com/ifyouseewendy/zhifou/master/example/ci.song.0.json')
+            .then(function(response) {
+                return response.json();
+            }).then(function(poems) {
+                console.log(poems);
+                const poem = getRandom(poems);
+                document.getElementById("title").innerHTML = poem.rhythmic;
+                document.getElementById("poet").innerHTML = poem.author;
+                document.getElementById("contents").innerHTML = poem.paragraphs.join("<br/>");
+            });
     },
     false
 );
