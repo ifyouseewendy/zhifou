bff9fd94c32020c794364b5e310c21c8c5d826e8
Di Wen <ifyouseewendy@gmail.com>
2019-02-18 21:57:17 -0500

    Cache peoms in chrome storage*
    - 1st load, display 知否
    - 2nd load, display 争渡
    - otherwise, display when there is a poem in storage, fetch from server
    if not

--- loader.js
+++ loader.js
@@ -3,23 +3,43 @@ document.addEventListener(
     function() {
         document.body.style.backgroundColor = getRandom(BACKGROUND_COLOR_LIST);
 
-        fetchPoems().then(
-            function(poems) {
-                const poem = getRandom(poems);
-                document.getElementById("title").innerHTML = poem.rhythmic;
-                document.getElementById("poet").innerHTML = poem.author;
-                document.getElementById("contents").innerText = poem.content;
-            }
-        );
+        getPoem().then(poem => renderPoem(poem));
     },
     false
 );
 
+function renderPoem(poem) {
+    document.getElementById("title").innerHTML = poem.rhythmic;
+    document.getElementById("poet").innerHTML = poem.author;
+    document.getElementById("contents").innerText = poem.content;
+}
+
+function getPoem() {
+    return new Promise(
+        function(resolve, reject) {
+            chrome.storage.sync.get(['poems'], function(result) {
+                const poems = (!result.poems || result.poems.length === 0) ?  [...DEFAULT_POEMS] : result.poems;
+                const poem = poems.shift();
+
+                if (poems.length === 0) {
+                    fetchPoems().then(function(poems) {
+                        chrome.storage.sync.set({ "poems": poems });
+                    });
+                } else {
+                    chrome.storage.sync.set({ "poems": poems });
+                }
+
+                resolve(poem);
+            })
+        }
+    )
+}
+
 function fetchPoems() {
-    const query = { query: "{  ci {    author    rhythmic    content  }}" };
+    const query = { query: "{ ci { author rhythmic content }}" };
 
     return fetch(
-        'https://chinese-poetry.ifyouseewendy.com/graphql',
+        'http://localhost:3000/graphql',
         {
             method: "POST",
             headers: { "Content-Type": "application/json" },
@@ -28,13 +48,11 @@ function fetchPoems() {
     ).then(
         function(response) {
             if (response.status == 200) {
-                return response.json();
+                return response.json().then(res => res.data.ci);
             } else {
-                return POEMS;
+                return DEFAULT_POEMS;
             }
         }
-    ).then(
-        (poems) => poems.data.ci
     );
 }
 
@@ -43,36 +61,18 @@ function getRandom(list) {
     return list[i];
 }
 
-const POEMS = {
-    data: {
-        ci: [
-            {
-                "author": "李清照",
-                "content": [
-                    "常记溪亭日暮。",
-                    "沈醉不知归路。",
-                    "兴尽晚回舟，误入藕花深处。",
-                    "争渡。",
-                    "争渡。",
-                    "惊起一滩鸥鹭。"
-                ],
-                "rhythmic": "如梦令"
-            },
-            {
-                "author": "李清照",
-                "content": [
-                    "昨夜雨疏风骤。",
-                    "浓睡不消残酒。",
-                    "试问卷帘人，却道海棠依旧。",
-                    "知否。",
-                    "知否。",
-                    "应是绿肥红瘦。"
-                ],
-                "rhythmic": "如梦令"
-            }
-        ]
+const DEFAULT_POEMS = [
+    {
+        "author": "李清照",
+        "content": "昨夜雨疏风骤，浓睡不消残酒。\n试问卷帘人，却道海棠依旧。\n知否？知否？\n应是绿肥红瘦。",
+        "rhythmic": "如梦令"
+    },
+    {
+        "author": "李清照",
+        "content": "常记溪亭日暮，沈醉不知归路。\n兴尽晚回舟，误入藕花深处。\n争渡，争渡，\n惊起一滩鸥鹭。",
+        "rhythmic": "如梦令"
     }
-};
+];
 
 const BACKGROUND_COLOR_LIST = [
     "#86a697",
